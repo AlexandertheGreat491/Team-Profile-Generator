12c134a5c5608f13bf9ad0c5ae6b89a95ff7a61a
agvan <agvandyke25@gmail.com>
2022-06-28 18:24:49 -0500

    started creating structure for html

--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,5 @@
 //template helper code
+
 //creates the header section of the page
 const generateHeader = headerText => {
     if (!headerText) {
@@ -9,4 +10,35 @@ const generateHeader = headerText => {
         <h1>My Team</h1>
         </header>
         `;
+}
+
+// creates team members roster
+
+const generateRoster = rosterArr => {
+    return `
+    <section>
+     <h2>Members</h2>
+    </section>
+    `;
+    ${rosterArr
+    .filter(({ member }) => member)
+    .map(({ name, position, ID, Email, Office, GitHub }) =>{
+        return `
+        <div>
+        <h3>${name}</h3>
+        <h4>${position}</h4>
+        <div>${ID}</div>
+        <div>${Email}</div>
+        <div>${Office}</div>
+        <div>${GitHub}</div>
+        </div>
+        `;
+    })
+    .join('')
+        
+}
+    
+    
+    
+    
 }
