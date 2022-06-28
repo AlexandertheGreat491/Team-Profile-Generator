dcf9ba20304fbc8fb1322709f5284f971aee1861
agvan <agvandyke25@gmail.com>
2022-06-28 17:45:45 -0500

    began writing the code to generate the html

--- src/page-template.js
+++ src/page-template.js
@@ -1,1 +1,12 @@
 //template helper code
+//creates the header section of the page
+const generateHeader = headerText => {
+    if (!headerText) {
+        return '';
+    }
+    return `
+        <header>
+        <h1>My Team</h1>
+        </header>
+        `;
+}
