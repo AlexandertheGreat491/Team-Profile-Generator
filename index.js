3f290b05d45f1ac372e2ca0c134d25ced95ed35c
agvan <agvandyke25@gmail.com>
2022-06-29 11:45:27 -0500

    added employee Init function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //packages imported
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generatePage = require ('./utils/generate-page');
+const generatePage = require('./utils/generate-page');
 const Manager = require('./lib/Manager.js');
 const Engineer = require('./lib/Engineer.js');
 const Intern = require('./lib/Intern.js');
@@ -21,3 +21,30 @@ const questions = {
     school: `What school does the intern attend?`
 };
 
+//user will be prompted for information
+
+const employeeInit = employeeArr => {
+    managerArr = employeeArr.filter(employeeArr => employeeArr.role === 'Manager');
+    engineerArr = employeeArr.filter(employeeArr => employeeArr.role === 'Engineer');
+    internArr = employeeArr.filter(employeeArr => employeeArr.role === 'Intern');
+
+    managerArr.forEach(manager => {
+        const parsedId = parseInt(manager.id);
+        const parsedOffice = parseInt(manager.officeNumber)
+        const managerItem = new Manager(manager.name, parseId, manager.email, parsedOffice);
+        totalArr.push(engineerItem);
+    });
+    engineerArr.forEach(engineer => {
+        const parsedId = parseInt(engineer.id);
+        const engineerItem = new Engineer(engineer.name, parseId, engineer.email, engineer.github);
+        totalArr.push(engineerItem);
+    });
+    internArr.forEach(intern => {
+        const parseId = parseInt(intern.id);
+        const internItem = new Intern(intern.name, parseId, intern.email, intern.school);
+        totalArr.push(internItem);
+    });
+    console.log('this is the array of everything');
+    console.log(totalArr);
+    console.log(totalArr[0].name);
+};
