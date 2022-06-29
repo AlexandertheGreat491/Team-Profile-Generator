0c2f8cfc7fe0f0a243eeee5c695f107800a51384
agvan <agvandyke25@gmail.com>
2022-06-29 16:15:09 -0500

    school test FAIL as expected

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,2 +1,8 @@
 //require statement
 const Intern = require("../lib/Intern");
+
+test("Can create school.", () => {
+    const testingSchool = "School Name";
+    const employeeInstance = new Intern("Alexander", 123, "agvandyke25@gmail.com", testingSchool);
+    expect(employeeInstance.school).toBe(testingSchool);
+});
