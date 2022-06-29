422b44c34aa538a876744647223f6e6ebd8195b8
agvan <agvandyke25@gmail.com>
2022-06-29 15:15:32 -0500

    testGithub test FAIL as expected

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,1 +1,9 @@
+
 const Engineer = require("../lib/Engineer.js");
+
+//testGithub test
+test("Can create a github.", () => {
+    const testGithub = "AlexandertheGreat491";
+    const employeeInstance = new Engineer("Alexander", 123, "agvandyke25@gmail.com", testGithub);
+    expect(employeeInstance.github).toBe(testGithub);
+});
