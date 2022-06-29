8a850f9b15952824af031c3ecd8b99635a0125b7
agvan <agvandyke25@gmail.com>
2022-06-28 21:08:47 -0500

    generateIntern(data) added

--- utils/generate-page.js
+++ utils/generate-page.js
@@ -33,3 +33,21 @@ function generateEngineer(data) {
     </div>
     `;
 };
+
+function generateIntern(data) {
+    return `
+    <div class="col s12 m6 14">
+      <div class="card">
+        <div class="card-panel purple">
+          <h2>${data.getName()}</h2>
+          <h3><i class="small material-icons">insert_school</i>${data.getRole()}</h1>
+        </div>
+        <div class="card-content">
+          <li>ID: ${data.getId()}</li>
+          <li>Email: <a href='mailto:${data.getEmail()}'>${data.getEmail()}</a></li>
+          <li>School: ${data.getSchool()}</li>
+        </div>
+       </div>
+       </div>
+    `;
+};
