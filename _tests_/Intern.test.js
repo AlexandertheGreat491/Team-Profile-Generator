9d4d54d565a84d375637483f18e8e7541e7eaf2a
agvan <agvandyke25@gmail.com>
2022-06-29 16:20:27 -0500

    getSchool() method test FAIL as expected

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,8 +1,17 @@
 //require statement
+
 const Intern = require("../lib/Intern");
 
+//school test
 test("Can create school.", () => {
     const testingSchool = "School Name";
     const employeeInstance = new Intern("Alexander", 123, "agvandyke25@gmail.com", testingSchool);
     expect(employeeInstance.school).toBe(testingSchool);
+});
+
+//getSchool() method test
+test("Testing officeNumber will return office number.", () => {
+    const testingSchool = "School Name";
+    const employeeInstance = new Intern("Alexander", 123, "agvandyke25@gmail.com", testingSchool);
+    expect(employeeInstance.getSchool()).toBe(testingSchool);
 });
