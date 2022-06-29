c83fb4b82195c8be0f511ed43d3cd4f084b40ae7
agvan <agvandyke25@gmail.com>
2022-06-28 20:57:15 -0500

    for loops generateEngineer/Interns

--- utils/generate-page.js
+++ utils/generate-page.js
@@ -13,3 +13,23 @@ function generateManager(data) {
     </div>
     </div>`;
 };
+
+// calling the generateEngineers/Interns
+
+function generateEngineer(data) {
+    return `
+    <div class="col s12 m6 14">
+      <div class="card">
+      <div class="card-panel purple">
+        <h2>${data.getName()}</h2>
+        <h3><i class="small material-icons">insert_laptop_windows</i>${data.getRole()}</h3>
+      </div>
+      <div class="card-content">
+      <li>ID: ${data.getId()}</li>
+      <li>Email: <a href='mailto:${getEmail()}'>${data.getEmail()}</a></li>
+      <li>GitHub: ${data.getGithub()}</li>
+      </div>
+    </div>
+    </div>
+    `;
+};
