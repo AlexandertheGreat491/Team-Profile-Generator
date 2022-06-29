564b403ed3488ca8df1e09f5d67f597e8eeef216
agvan <agvandyke25@gmail.com>
2022-06-29 13:13:32 -0500

    initialization test FAIL as expected

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,1 +1,16 @@
 const Employee = require('../lib/Employee');
+
+
+describe('Employee', () =>{
+    let employee;
+    beforeEach(() =>{
+        employee = new Employee('Alexander G. Van Dyke', 123, 'agvandyke25@gmail.com');
+    })
+    describe ('Intialization of Employee', () => {
+        it('should create an object with a name, id, and email if provided valid arguments', () => {
+            expect(employee.name).toEqual('Alexander G. Van Dyke');
+            expect(employee.id).toEqual(123);
+            expect(employee.email).toEqual('agvandyke25@gmail.com');
+        });
+    })
+})
