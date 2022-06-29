b8dd3ab54df3fced467da081730724709c55ecf6
agvan <agvandyke25@gmail.com>
2022-06-29 15:27:18 -0500

    getRole() method test FAIL as expected

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -14,3 +14,10 @@ test("Testing getGithub will return github.", () => {
     const employeeInstance = new Engineer("Alexander", 123, "agvandyke25@gmail.com", testGithub);
     expect(employeeInstance.getGithub()).toBe(testGithub);
 });
+
+//getRole() method test
+test("Testing role.", () => {
+    const returnAnswer = "Engineer";
+    const employeeInstance = new Engineer("Alexander", 123, "agvandyke25@gmail.com", "AlexandertheGreat491");
+    expect(employeeInstance.getRole()).toBe(returnAnswer);
+});
