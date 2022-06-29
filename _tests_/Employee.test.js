18b226cd32e9efedd2bd608bf5f0f6d92f01185d
agvan <agvandyke25@gmail.com>
2022-06-29 13:53:39 -0500

    name test PASS

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,8 +1,16 @@
 
+
 const Employee = require('../lib/Employee.js');
 
 
 test ("Can create a new employee.", () => {
     const employeeInstance = new Employee();
     expect(typeof(employeeInstance)).toBe("object");
+})
+
+//name test
+test("Testing name.", () => {
+    const name = "Alexander";
+    const employeeInstance = new Employee(name);
+    expect(employeeInstance.name).toBe(name);
 })
