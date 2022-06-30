7a86f38c616740509bb009004bb4b8ce679a6710
agvan <agvandyke25@gmail.com>
2022-06-30 09:50:44 -0500

    getRole() test FAIL as expected

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,5 +1,4 @@
 //require statement
-
 const Manager = require("../lib/Manager");
 
 //tests
@@ -16,4 +15,11 @@ test("Testing OfficeNumber will return office number.", () => {
     const testOfficeNumber = 123;
     const employeeInstance = new Manager("Alexander", 123, "agvandyke25@gmail.com", testOfficeNumber);
     expect(employeeInstance.getOfficeNumber()).toBe(testOfficeNumber);
+});
+
+//getRole() method test
+test("Testing role.", () => {
+    const returnAnswer = "Manager";
+    const employeeInstance = new Manager("Alexander", 123, "agvandyke25@gmail.com", 123);
+    expect(employeeInstance.getRole()).toBe(returnAnswer);
 });
