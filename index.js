006f0fe0b8fdc88dc3c4fe6389a7aa712660123d
agvan <agvandyke25@gmail.com>
2022-06-30 11:11:44 -0500

    changed app initialization functions

--- index.js
+++ index.js
@@ -1,123 +1,140 @@
-//packages imported
+//importing packages
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generatePage = require('./utils/generate-page');
+const generatePage = require('./utils/generatePage.js');
 const Manager = require('./lib/Manager.js');
 const Engineer = require('./lib/Engineer.js');
 const Intern = require('./lib/Intern.js');
-
-// stores employee info from prompts
+//storing employee info from prompts
 let employeeArr = [];
 let totalArr = [];
 let managerArr = [];
 let engineerArr = [];
 let internArr = [];
 const questions = {
-    name: `What is the name of the employee?`,
-    employeeID: `What is the ID of the employee?`,
+    name: `What is the employee's name?`,
+    employeeID: `What is the employee's ID?`,
     email: `What is the employee's email?`,
     officeNumber: `What is the office number?`,
-    github: `What is the GitHub ID of the employee?`,
+    github: `What is the GitHub ID of the engineer?`,
     school: `What school does the intern attend?`
 };
+// prompting user for information
 
-//user will be prompted for information
-
-const employeeInit = employeeArr => {
-    managerArr = employeeArr.filter(employeeArr => employeeArr.role === 'Manager');
-    engineerArr = employeeArr.filter(employeeArr => employeeArr.role === 'Engineer');
-    internArr = employeeArr.filter(employeeArr => employeeArr.role === 'Intern');
+const employeeInit = empArr => {
+    managerArr = empArr.filter(empArr => empArr.role === 'Manager');
+    engineerArr = empArr.filter(empArr => empArr.role === 'Engineer');
+    internArr = empArr.filter(empArr => empArr.role === 'Intern');
 
     managerArr.forEach(manager => {
         const parsedId = parseInt(manager.id);
         const parsedOffice = parseInt(manager.officeNumber)
-        const managerItem = new Manager(manager.name, parseId, manager.email, parsedOffice);
-        totalArr.push(engineerItem);
+        const managerItem = new Manager(manager.name, parsedId, manager.email, parsedOffice);
+        totalArr.push(managerItem);
     });
     engineerArr.forEach(engineer => {
         const parsedId = parseInt(engineer.id);
-        const engineerItem = new Engineer(engineer.name, parseId, engineer.email, engineer.github);
+        const engineerItem = new Engineer(engineer.name, parsedId, engineer.email, engineer.github);
         totalArr.push(engineerItem);
     });
     internArr.forEach(intern => {
-        const parseId = parseInt(intern.id);
-        const internItem = new Intern(intern.name, parseId, intern.email, intern.school);
+        const parsedId = parseInt(intern.id);
+        const internItem = new Intern(intern.name, parsedId, intern.email, intern.school);
         totalArr.push(internItem);
     });
-    console.log('this is the array of everything');
+    console.log('this is the final array')
     console.log(totalArr);
     console.log(totalArr[0].name);
-};
 
-//questions that will prompt user for information
+};
 const promptUser = () => {
     return inquirer.prompt([{
-        type: 'list',
-        name: 'role',
-        message: questions.employeeType,
-        choices: ['Manager', 'Engineer', 'Intern', 'Finished adding teammates']
-    }, {
-        type: 'input',
-        name: 'name',
-        message: questions.name,
-        when: (answers) => answers.role !== 'Finished adding teammates'
-    }, {
-        type: 'input',
-        name: 'id',
-        message: questions.employeeID,
-        when: (answers) => answers.role !== 'Finished adding teammates'
-    }, {
-        type: 'input',
-        name: 'email',
-        message: questions.email,
-        when: (answers) => answers.role !== 'Finished adding teammates'
-    }, {
-        type: 'input',
-        name: 'officeNumber',
-        message: questions.github,
-        when: (answers) => answers.role === 'Manager'
-    }, {
-        type: 'input',
-        name: 'github',
-        message: questions.github,
-        when: (answers) => answers.role === 'Engineer'
-    }, {
-        type: 'input',
-        name: 'school',
-        message: questions.school,
-        when: (answers) => answers.role === 'Intern'
-    },
+            // This question is only asked when the license question is selected as yes
+            type: 'list',
+            name: 'role',
+            message: questions.employeeType,
+            choices: ['Manager', 'Engineer', 'Intern', 'Finished adding teammates']
+        },
+         {
+            type: 'input',
+            name: 'name',
+            message: questions.name,
+            when: (answers) => answers.role !== 'Finished adding teammates'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: questions.employeeID,
+            when: (answers) => answers.role !== 'Finished adding teammates'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: questions.email,
+            when: (answers) => answers.role !== 'Finished adding teammates'
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: questions.officeNumber,
+            when: (answers) => answers.role === 'Manager'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: questions.github,
+            when: (answers) => answers.role === 'Engineer'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: questions.school,
+            when: (answers) => answers.role === 'Intern'
+        },
 
 
-    ])
-        .then((answers) => {
-            if (answers.role === 'Finished adding teammates') {
-                employeeArr.push(answers);
-                console.log('THis is the finished employee array prior')
-                console.log(employeeArr);
-                console.log(`End of Employee Question`);
-                console.log(employeeArr[0]);
-                employeeInit(employeeArr);
-            } else {
-                employeeArr.push(answers);
-                console.log(answers);
-                console.log(`adding more employees`);
-                console.log(employeeArr);
-                return promptUser();
-            }
-        });
-
+    ]).then((answers) => {
+        if (answers.role === 'Finished adding teammates') {
+            employeeArr.push(answers);
+            console.log('This is the finished employee array prior')
+            console.log(employeeArr);
+            console.log(`End of Employee Question`);
+            console.log(employeeArr[0]);
+            employeeInit(employeeArr);
+        } else {
+            employeeArr.push(answers);
+            // console.log(answers);
+            console.log(`adding more employees`);
+            console.log(employeeArr);
+            return promptUser();
+        }
+    });
 };
 
-//This function will initialize the app.
-const init = () => {
-    promptUser()
-    //writeFileSync method uses promises instead of a callback function
-    .then((() => fs.writeFileSync('./dist/index.html', generatePage(totalArr))))
-
-    .then(() => console.log('Successfully wrote to index.html'))
-    .catch((err) => console.error(err));
+//Function writes the html file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err){
+        if (err) throw err;
+        console.log('testindex.html file created!');
+    })
 };
 
-//This function call will initialize the app.
+//const init = () => {
+    //promptUser()
+        // .then(console.log("end of init"))
+        // Use writeFileSync method to use promises instead of a callback function
+        //.then((() => fs.writeFileSync('./dist/index.html', generatePage(totalArr))))
+
+    //.then(() => console.log('Successfully wrote to index.html'))
+        //.catch((err) => console.error(err));
+//};
+
+// function to initialize app
+function init() {
+    inquirer.prompt(questions).then(function (data){
+        writeToFile("testindex.html", generatePage(data))
+    });
+};
+
+// Function call to initialize app
 init();
