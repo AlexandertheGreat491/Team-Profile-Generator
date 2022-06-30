8ac7cfaccfea0ab44d7e2a94afeba518aadfc58b
agvan <agvandyke25@gmail.com>
2022-06-30 09:33:25 -0500

    Office Number test FAIL as expected

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,2 +1,12 @@
 //require statement
+
 const Manager = require("../lib/Manager");
+
+//tests
+
+//OfficeNumber test
+test("Can create an office number.", () => {
+    const testOfficeNumber = 2;
+    const employeeInstance = new Manager("Alexander", 2, "agvandyke25@gmail.com", testOfficeNumber);
+    expect(employeeInstance.officeNumber).toBe(testOfficeNumber);
+});
